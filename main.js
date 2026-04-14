95f8f252d02f0bb81df5c7351c4e6b55ae07d5b2
Devraj Basnet <67828453+devrajbasnet@users.noreply.github.com>
2026-04-14 12:16:38 +0545

    main.js

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-const bxMenu = document.querySelector('.bx.bx-menu'); // Corrected selector
-const ul = document.querySelector('ul');
 
-bxMenu.addEventListener('click', () => {
-    ul.classList.toggle('ham_active');
-
-    let hamChild = bxMenu.firstElementChild;
-    if (ul.classList.contains('ham_active')) {
-        hamChild.classList.replace('fa-bars', 'fa-xmark');
-    } else {
-        hamChild.classList.replace('fa-xmark', 'fa-bars');
-    }
-});
